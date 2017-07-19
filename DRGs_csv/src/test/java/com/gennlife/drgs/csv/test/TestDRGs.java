0b6d3a86ca318213ebc4bf1f8f0bc01cb4febe40
wangmiao1218 <wangmiao1218@163.com>
2017-07-19 14:45:55 +0800

    format

--- DRGs_csv/src/test/java/com/gennlife/drgs/csv/test/TestDRGs.java
+++ DRGs_csv/src/test/java/com/gennlife/drgs/csv/test/TestDRGs.java
@@ -3,17 +3,14 @@ package com.gennlife.drgs.csv.test;
 import org.junit.Test;
 import com.gennlife.drgs.csv.DRGs;
 
-
 public class TestDRGs {
-	
+
 	private static String filePath = "F:\\DRGs\\newadd";
 
-    @Test  
-    public  void testDrgs(){  
-    	DRGs.drgs(filePath);
-    	System.out.println("OK");
-    }  
-	
-	
-	
+	@Test
+	public void testDrgs() {
+		DRGs.drgs(filePath);
+		System.out.println("OK");
+	}
+
 }
