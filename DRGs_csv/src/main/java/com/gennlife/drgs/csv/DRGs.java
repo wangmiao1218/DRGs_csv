ce9d7dfd74121e38ccd936fd7ce08a11be42da35
wangmiao1218 <wangmiao1218@163.com>
2017-07-19 14:44:35 +0800

    format

--- DRGs_csv/src/main/java/com/gennlife/drgs/csv/DRGs.java
+++ DRGs_csv/src/main/java/com/gennlife/drgs/csv/DRGs.java
@@ -10,36 +10,35 @@ import com.gennlife.drgs.utils.ListAndStringUtils;
 /**
  * @Description: 处理csv文件
  * @author: wangmiao
- * @Date: 2017年7月19日 下午2:14:36 
+ * @Date: 2017年7月19日 下午2:14:36
  */
 public class DRGs {
-	
-	/** 
-	* @Title: drgs 
-	* @Description: 
-	* @param: @param filePath :
-	* @return: void
-	* @throws 
-	*/
-	public static void drgs(String filePath) {
-		//获取目录下所有文件
-		ArrayList<File> files=FileUtils.getListFiles(filePath);  
-		//将其转成list
-	    List<String> list = FileUtils.arrayFilesToListString(files);
-	    
-	    //循环遍历list，分别读入和写入
-	    for (int i = 0; i < list.size(); i++) {
 
-        	String readFilePath = list.get(i);
-			
-			//处理路径变成文件名
+	/**
+	 * @Title: drgs
+	 * @Description:
+	 * @param: @param filePath :
+	 * @return: void
+	 * @throws
+	 */
+	public static void drgs(String filePath) {
+		// 获取目录下所有文件
+		ArrayList<File> files = FileUtils.getListFiles(filePath);
+		// 将其转成list
+		List<String> list = FileUtils.arrayFilesToListString(files);
+
+		// 循环遍历list，分别读入和写入
+		for (int i = 0; i < list.size(); i++) {
+
+			String readFilePath = list.get(i);
+
+			// 处理路径变成文件名
 			String str = ListAndStringUtils.stringToSubstring(list.get(i));
-			//拼接成写入路径
-			String writeFilePath = "F:\\DRGs\\test\\"+str+"_1.csv";
-			
+			// 拼接成写入路径
+			String writeFilePath = "F:\\DRGs\\test\\" + str + "_1.csv";
+
 			CsvReadAndWrite.readAndWriteCsv(readFilePath, writeFilePath);
-	    }
+		}
 	}
-	
-	
+
 }
