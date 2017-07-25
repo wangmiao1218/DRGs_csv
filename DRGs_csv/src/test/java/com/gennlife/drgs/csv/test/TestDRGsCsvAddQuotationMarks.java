e94967a1709573b42b6ca72a189214ba04b7ee8c
wangmiao1218 <wangmiao1218@163.com>
2017-07-25 14:45:43 +0800

    drgsCsvAddQuotationMarksByFilePath

--- DRGs_csv/src/test/java/com/gennlife/drgs/csv/test/TestDRGsCsvAddQuotationMarks.java
+++ DRGs_csv/src/test/java/com/gennlife/drgs/csv/test/TestDRGsCsvAddQuotationMarks.java
@@ -8,8 +8,8 @@ public class TestDRGsCsvAddQuotationMarks {
 	//private static String readFilePath = "F:\\DRGs\\newadd";
 	//private static String writeFilePath = "F:\\DRGs\\test\\";
 	
-	private static String readFilePath = "E:\\武汉DRGS\\测试\\6_7.22\\7.22";
-	private static String writeFilePath = "E:\\武汉DRGS\\测试\\6_7.22\\7.22_add\\";
+	private static String readFilePath = "E:\\武汉DRGS\\测试\\6_7.22\\7.22_z";
+	private static String writeFilePath = "E:\\武汉DRGS\\测试\\6_7.22\\7.22_z_add\\";
 
 	@Test
 	public void drgsCsvAddQuotationMarksByFilePath() {
